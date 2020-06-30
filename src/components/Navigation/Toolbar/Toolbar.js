c86c7813f0f2326caa9c8c8795e20da71ec93a4e
lukasmyth96 <luka@nate.tech>
2020-06-30 08:15:05 +0100

    finished menu toggle

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -2,12 +2,11 @@ import React from 'react';
 import classes from './Toolbar.module.css'
 import Logo from '../../Logo/Logo'
 import NavigationItems from '../NavigationItems/NavigationItems'
+import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle'
 
 const toolbar = (props) => (
     <header className={classes.Toolbar}>
-        <div>
-            <button onClick={props.showSideDrawer}> menu </button>
-        </div>
+        <DrawerToggle showSideDrawer={props.showSideDrawer} />
         <div className={classes.Logo}>
             <Logo/>
         </div>
