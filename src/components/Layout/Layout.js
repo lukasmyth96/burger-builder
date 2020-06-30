41a15d3216caf5d033def2a4becb9be98504d4a1
lukasmyth96 <luka@nate.tech>
2020-06-30 08:05:22 +0100

    added sideDrawer toggle button

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,17 +1,33 @@
-import React from 'react'
-import Aux from '../../hoc/Aux'
+import React, { Component } from 'react'
 import classes from './Layout.module.css'
 import Toolbar from '../Navigation/Toolbar/Toolbar'
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
 
-const layout = (props) => (
-    <Aux>
-        <Toolbar/>
-        <SideDrawer/>
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Aux>
-);
+class Layout extends Component {
 
-export default layout;
+    state = {
+        sideDrawerVisible: false
+    }
+
+    toggleSideDrawerHandler = () => {
+        // NOTE - this is the correct way to change state when it depends on the previous state as setState runs async
+        this.setState( ( prevState ) => {
+            return { sideDrawerVisible: !prevState.sideDrawerVisible };
+        } );
+    }
+
+    render() {
+        return (
+        <>
+            <Toolbar showSideDrawer={this.toggleSideDrawerHandler}/>
+            <SideDrawer open={this.state.sideDrawerVisible} closeSideDrawer={this.toggleSideDrawerHandler}/>
+            <main className={classes.Content}>
+                {this.props.children}
+            </main>
+        </>
+        );
+    }
+}
+
+
+export default Layout;
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -5,11 +5,13 @@ import NavigationItems from '../NavigationItems/NavigationItems'
 
 const toolbar = (props) => (
     <header className={classes.Toolbar}>
-        <div>MENU</div>
+        <div>
+            <button onClick={props.showSideDrawer}> menu </button>
+        </div>
         <div className={classes.Logo}>
             <Logo/>
         </div>
-        <nav>
+        <nav className={classes.DesktopOnly}>
             <NavigationItems/>
         </nav>
     </header>
